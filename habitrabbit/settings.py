23443ae4848a490793baefe1bfd827f13d5a3c4a
rudisiona <rsmageo@gmail.com>
2025-11-04 16:28:07 -0800

    typo fix

--- habitrabbit/settings.py
+++ habitrabbit/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getnev('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
